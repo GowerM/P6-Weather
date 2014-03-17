38891d4a7c0e439b68ed071078921ece8ca6614a
Mollie <eg.mollie@gmail.com>
2014-03-17 14:11:24 -0700

    version 4

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,39 @@
 // Your Awesome Scripts!
 $(document).ready(function(){
 
-  var zip = '99004';
+  //var zip = '99004';
   //console.log(zip);
 
   $.simpleWeather({
     
+    /*
+    
+    // Get _weather_ object
+    success: function(weather) {
+      
+      // Get & store temperature
+      var temp = weather.temp;
+      // Get & store city
+      var city = weather.city;
+      
+      // Output to hooks in HTML
+      $('.cheney .temp').text(temp);
+      $('.cheney .city').text(city);*/
+
+
     location: '99004',
+    unit: 'F',
     
     success: function(weather) {
       
+      
+
       // Get & Store Weather Data
       html = '<h2><i class="icon-' + weather.code+'"></i> ' + weather.temp +'&deg;' + weather.units.temp+'</h2>';
       
       var temp, tomHi, tomLo, cityAndState, tomForecast, conditionCode; 
 
-      temp = weather.temp + '<span> F</span>';
+      temp = weather.temp;
       tomHi = weather.tomorrow.high;
       tomLo = weather.tomorrow.low;
       cityAndState = weather.city + ', ' + weather.region;
@@ -30,11 +48,11 @@ $(document).ready(function(){
       //console.log(cityAndState);
 
       // Display Weather
-      $('.temp').html(temp);
-      $('.tomHi').html(tomHi);
-      $('.tomLo').html(tomLo);
-      $('.cityAndState').html(cityAndState);
-      $('.tomForecast').html(weather.tomorrow.forecast);
+      $('.cheney .temp').text(temp);
+      $('.cheney .tomHi').text(tomHi);
+      $('.cheney .tomLo').text(tomLo);
+      $('.cheney .cityAndState').text(cityAndState);
+      $('.cheney .tomForecast').text(weather.tomorrow.forecast);
       $('body').addClass('bg' + conditionCode);
 
     },
@@ -44,6 +62,89 @@ $(document).ready(function(){
     }
   
   });
+
+$.simpleWeather({
+
+
+    location: '98101',
+    unit: 'F',
+    
+    success: function(weather) {
+      
+      // Get & Store Weather Data
+      html = '<h2><i class="icon-' + weather.code+'"></i> ' + weather.temp +'&deg;' + weather.units.temp+'</h2>';
+      
+      var temp, tomHi, tomLo, cityAndState, tomForecast, conditionCode; 
+
+      temp = weather.temp;
+      tomHi = weather.tomorrow.high;
+      tomLo = weather.tomorrow.low;
+      cityAndState = weather.city + ', ' + weather.region;
+      tomForecast = weather.tomorrow.forecast;
+      conditionCode = weather.code;
+
+        console.log(conditionCode);
+
+      //console.log(cityAndState);
+
+      // Display Weather
+      $('.seattle .temp').html(temp);
+      $('.seattle .tomHi').html(tomHi);
+      $('.seattle .tomLo').html(tomLo);
+      $('.seattle .cityAndState').html(cityAndState);
+      $('.seattle .tomForecast').html(weather.tomorrow.forecast);
+      $('body').addClass('bg' + conditionCode);
+
+    },
+    
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  
+  });
+
+// REF: http://foundation.zurb.com/docs/
+// REF: http://simpleweatherjs.com/
+
+// On click button, get zip, then run Simple Weather
+$('#getWeather button').on('click', function() {
+  
+  // 1. Get & store entered zipcode
+  var zipcode = $('#getWeather input').val();
+  
+  // 2. Pass weather into _simpleWeather()_ object
+  $.simpleWeather({
+    //Put multiple var in an array with square brackets
+    
+    location: zipcode,
+  
+    success: function(weather) {
+      
+      // Get & store temperature
+      var temp = weather.temp;
+      // Get & store city
+      var city = weather.city;
+      
+      // Output to hooks in HTML
+      $('.temp').text(temp);
+      $('.city').text(city);
+
+      // See console for all properties of object
+      console.log(weather);
+    },
+  
+    error: function(error) {
+      $('body').html('<p>' + error + '</p>');
+    }
+  
+  });
+  
+  // 3. Reset input value
+  //This clears the search bar
+$('#getWeather input').val('');
+  
+});
+
 
 
 
