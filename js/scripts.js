0ccd6d82a18b49eabef0459902d458c73c594698
Mollie <eg.mollie@gmail.com>
2014-03-17 13:20:23 -0700

    version 3

--- js/scripts.js
+++ js/scripts.js
@@ -4,29 +4,38 @@
 // Your Awesome Scripts!
 $(document).ready(function(){
 
+  var zip = '99004';
+  //console.log(zip);
+
   $.simpleWeather({
     
-    location: '99203',
+    location: '99004',
     
     success: function(weather) {
       
       // Get & Store Weather Data
       html = '<h2><i class="icon-' + weather.code+'"></i> ' + weather.temp +'&deg;' + weather.units.temp+'</h2>';
       
-      var temp, tomHi, tomLo, cityAndState; 
+      var temp, tomHi, tomLo, cityAndState, tomForecast, conditionCode; 
 
       temp = weather.temp + '<span> F</span>';
       tomHi = weather.tomorrow.high;
       tomLo = weather.tomorrow.low;
       cityAndState = weather.city + ', ' + weather.region;
+      tomForecast = weather.tomorrow.forecast;
+      conditionCode = weather.code;
 
-      console.log(cityAndState);
+        console.log(conditionCode);
+
+      //console.log(cityAndState);
 
       // Display Weather
       $('.temp').html(temp);
       $('.tomHi').html(tomHi);
       $('.tomLo').html(tomLo);
       $('.cityAndState').html(cityAndState);
+      $('.tomForecast').html(weather.tomorrow.forecast);
+      $('body').addClass('bg' + conditionCode);
 
     },
     
